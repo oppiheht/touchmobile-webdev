6c18d6ac55203e1df84dac30010b5f258aa268e2
oppiheht <oppiheht@gmail.com>
2016-09-27 16:25:03 -0700

    Broke out form validation into a separate function

--- content.js
+++ content.js
@@ -9,39 +9,40 @@ function menuLoaded() {
 }
 
 function surveyLoaded() {
+    $('.error-text').hide();
+    $('#start-chat-btn').click(validateForm);
+}
+
+function validateForm() {
     var nameInput = $('#name');
     var nameRequiredText = $('#nameRequiredText');
     var phoneInput = $('#phone');
     var phoneRequiredText = $('#phoneRequiredText');
     var phoneInvalidText = $('#phoneInvalidText');
     var phoneRegex = /\d{3}-\d{3}-\d{4}/
-    $('.error-text').hide();
-    $('#start-chat-btn').click(function () {
-        if (nameInput.val().length == 0) {
-            nameInput.addClass('invalid');
-            nameRequiredText.show();
-        }
-        else {
-            nameInput.removeClass('invalid');
-            $('#nameRequiredText').hide();
-        }
-        if (phoneInput.val().length == 0) {
-            phoneInput.addClass('invalid');
-            phoneRequiredText.show();
-        }
-        else if (!phoneInput.val().match(phoneRegex)) {
-            phoneInput.addClass('invalid');
-            phoneRequiredText.hide();
-            phoneInvalidText.show();
-        }
-        else {
-            phoneInput.removeClass('invalid');
-            phoneRequiredText.hide();
-            phoneInvalidText.hide();
-        }
-        
-        if (! (nameInput.hasClass('invalid') || phoneInput.hasClass('invalid'))) {
-            alert("succes");
-        }
-    });
+    if (nameInput.val().length == 0) {
+        nameInput.addClass('invalid');
+        nameRequiredText.show();
+    }
+    else {
+        nameInput.removeClass('invalid');
+        $('#nameRequiredText').hide();
+    }
+    if (phoneInput.val().length == 0) {
+        phoneInput.addClass('invalid');
+        phoneRequiredText.show();
+    }
+    else if (!phoneInput.val().match(phoneRegex)) {
+        phoneInput.addClass('invalid');
+        phoneRequiredText.hide();
+        phoneInvalidText.show();
+    }
+    else {
+        phoneInput.removeClass('invalid');
+        phoneRequiredText.hide();
+        phoneInvalidText.hide();
+    }
+    if (!(nameInput.hasClass('invalid') || phoneInput.hasClass('invalid'))) {
+        alert("succes");
+    }
 }
